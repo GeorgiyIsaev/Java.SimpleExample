45b71a058ebcbef0680ed543c57dd44c128c53a6
georgiyelbaf@gmail.com <georgiyelbaf@gmail.com>
2025-03-28 19:44:10 +0300

    Read

--- src/main/java/com/SimpleExample/ReadWritiStrings/Main.java
+++ src/main/java/com/SimpleExample/ReadWritiStrings/Main.java
@@ -3,26 +3,48 @@ package com.SimpleExample.ReadWritiStrings;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Scanner;
 /*Чтение и запись текста в файл*/
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        //  Scanner scanner = new Scanner(System.in);
-        // String input = scanner.nextLine();
+        /*Путь к файлу*/
+        String pathWindows = "C:\\myTest\\testFile.txt";
+        String pathWindowsInApp = "myTestFolder\\newFolder\\testFile.txt";
+        String separator = File.separator; //возвращает слеш соответсвующий OS
+        String pathWindowsInCrossApp =
+                "myTestFolder" + separator + "newFolder" +
+                        separator + "testFile2.txt";
 
-        /*Запись*/
-        File file = new File("testFile.txt");
-        PrintWriter pw = new PrintWriter(file);
+
+        /*Запись - пересоздает файл*/
+        File fileWrite = new File(pathWindowsInCrossApp);
+        PrintWriter pw = new PrintWriter(fileWrite);
         pw.println("Строка номер 1");
         pw.println("Строка номер 2");
+        pw.println("Строка номер 3");
         pw.close();
 
         /*Чтение*/
+        File fileRead = new File(pathWindowsInCrossApp);
+        Scanner scanner = new Scanner(fileRead);
+        while (scanner.hasNextLine()){
+            System.out.println(scanner.nextLine());
+        }
+        scanner.close();
 
+        /*Чтение 2*/
+        PrintWriter pw2 = new PrintWriter(new File(pathWindowsInApp));
+        pw2.println("First second third");
+        pw2.close();
 
+        Scanner scanner2 = new Scanner(new File(pathWindowsInApp));
+        String line = scanner2.nextLine();
 
-
+        String[] words = line.split(" ");
+        System.out.println(Arrays.toString(words));
+        scanner2.close();
     }
 
 }
