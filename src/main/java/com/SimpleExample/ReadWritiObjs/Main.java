f3ec44aeb014625e615d0011ac1558b50ee750ee
georgiyelbaf@gmail.com <georgiyelbaf@gmail.com>
2025-03-28 20:27:47 +0300

    Serializable Arr

--- src/main/java/com/SimpleExample/ReadWritiObjs/Main.java
+++ src/main/java/com/SimpleExample/ReadWritiObjs/Main.java
@@ -1,6 +1,7 @@
 package com.SimpleExample.ReadWritiObjs;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Main {
@@ -9,6 +10,16 @@ public class Main {
         WriteObject();
         /*Десериализация*/
         ReadObject();
+
+
+        System.out.println(" ");
+        WriteObjectArray();
+        ReadObjectArray();
+
+        System.out.println(" ");
+        WriteObjectArrayObj();
+        ReadObjectArrayObj();
+
     }
 
     private static void WriteObject(){
@@ -45,7 +56,70 @@ public class Main {
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void WriteObjectArray(){
+        Person[] peoples = {new Person(101,"Alisa"),
+                new Person(102,"Lisa"),
+                new Person(103,"Natasha" )};
+        try {
+            FileOutputStream fos = new FileOutputStream("people2.bin");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeInt(peoples.length);
+            for(Person person : peoples){
+                oos.writeObject(person);
+            }
+            oos.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void ReadObjectArray(){
+        try {
+            FileInputStream fis = new FileInputStream("people2.bin");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            int personCount = ois.readInt();
+            Person[] persons = new Person[personCount];
+
+            for(int i=0; i<personCount; i++){
+                persons[i] =   (Person)ois.readObject();
+            }
+            System.out.println((Arrays.toString(persons)));
+
+            ois.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void WriteObjectArrayObj(){
+        Person[] peoples = {new Person(101,"Alisa"),
+                new Person(102,"Lisa"),
+                new Person(103,"Natasha" )};
+        try {
+            FileOutputStream fos = new FileOutputStream("people3.bin");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(peoples);
+            oos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
+    private static void ReadObjectArrayObj(){
+        try {
+            FileInputStream fis = new FileInputStream("people3.bin");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            Person[] persons =  (Person[]) ois.readObject();
+            System.out.println((Arrays.toString(persons)));
+
+            ois.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
