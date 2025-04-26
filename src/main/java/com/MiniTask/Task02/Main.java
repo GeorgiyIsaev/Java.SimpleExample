cce88ca5db759ca3ae7d137ad4a34729cd5e84a4
georgiyelbaf@gmail.com <georgiyelbaf@gmail.com>
2025-04-26 23:09:40 +0300

    add Task02 (2)

--- src/main/java/com/MiniTask/Task02/Main.java
+++ src/main/java/com/MiniTask/Task02/Main.java
@@ -1,7 +1,20 @@
 package com.MiniTask.Task02;
 
+import com.MiniTask.Task01.Figure.Figure;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        //1
+        ConsolePrinter console = new ConsolePrinter();
+        console.print("Строка 1; ");
+        console.println("Строка 2; ");
+        //2
+        List<Figure> figures = new ArrayList<>();
+        figures.add(ShapeFactory.ofRectangle(2,4));
+        figures.add(ShapeFactory.ofSquare(5));
+        console.println(figures.toString());
     }
 }
