66ee18c90819f86ae4263b19092ee4922c4fc4ac
georgiyelbaf@gmail.com <georgiyelbaf@gmail.com>
2025-05-15 00:38:54 +0300

    [class ParameterizedTests] проверка теста данных вводимы в консоль

--- src/test/java/researchOnJUnit5Tests/ConsoleInputTest.java
+++ src/test/java/researchOnJUnit5Tests/ConsoleInputTest.java
@@ -9,8 +9,6 @@ import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
 import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -18,9 +16,12 @@ import java.util.stream.Stream;
 
 public class ConsoleInputTest {
 
+    /// Вводит что-то в консоль
     public void setUp(String command) {
         System.setIn(new ByteArrayInputStream(command.getBytes()));
     }
+
+    ///Простой пример ввода
     public String input(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите команду: ");
@@ -37,7 +38,7 @@ public class ConsoleInputTest {
         System.out.println("Ведена команда: " + userInput);
     }
 
-
+    /// Тестирование метода с одним вводом
     public static class MyClassArgumentsProvider implements ArgumentsProvider {
         @Override
         public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
@@ -62,8 +63,43 @@ public class ConsoleInputTest {
             System.out.println("Ведена команда: " + userInput);
 
         }
+    }
 
-
+    ///  Тестирование метода с несколькими вводами
+    public void inputPlus(int countCommand){
+        String command = "Количество команд: " + countCommand;
+        command+="\n";
+        Scanner scanner = new Scanner(System.in);
+        for(int i = 0; i<countCommand; i++){
+            System.out.print("Введите команду " + i +": ");
+            command += scanner.nextLine();
+            command+="--->>";
+        }
+        System.out.println(command);
+    }
+    public static class MyClassMultipleInputsArgumentsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) {
+            return Stream.of(
+                    Arguments.of(Arrays.asList("БлокA", "БлокB>")),
+                    Arguments.of(Arrays.asList("Команда1", "Команда2", "Команда3")),
+                    Arguments.of(Arrays.asList("0001", "0002", "0003", "0004" , "0005"))
+            );
+        }
+    }
+    @ParameterizedTest(name = "Тест метода ввода с двумя вводами")
+    @ArgumentsSource(MyClassMultipleInputsArgumentsProvider.class)
+    public void multipleInputsTest(List<String> commands){
+        String commandsFull = "";
+          if(!commands.isEmpty()) {
+              commandsFull = commands.get(0);
+          }
+        for(int i = 1; i<commands.size(); i++) {
+            commandsFull +="\n";
+            commandsFull += commands.get(i);
+        }
+        setUp(commandsFull);
+        inputPlus(commands.size());
     }
 
 }
