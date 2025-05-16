ecf0937853269b59991e0a087764fef4c8b48a61
georgiyelbaf@gmail.com <georgiyelbaf@gmail.com>
2025-05-16 22:32:23 +0300

    [MiniTasks 03] class MiniList<T> for task 01

---
+++ src/main/java/com/miniTask/task03/main/Task01_MiniList.java
@@ -0,0 +1,24 @@
+package com.miniTask.task03.main;
+
+import com.miniTask.task03.miniList.MiniList;
+
+public class Task01_MiniList {
+    public static void main(String[] args) {
+        MiniList<Integer> miniList = new MiniList<Integer>();
+        System.out.println("miniList: " + miniList);
+
+        miniList.addLast(100);
+        miniList.addLast(750);
+        miniList.addLast(44);
+        miniList.addLast(657);
+        miniList.addLast(125);
+        System.out.println("miniList for added: " + miniList);
+
+        while (miniList.getSize() > 1) {
+            Integer value = miniList.removeFirst();
+            System.out.println("removeFirst value: " + value);
+            System.out.println("miniList for removeFirst: " + miniList);
+        }
+
+    }
+}
---
+++ src/main/java/com/miniTask/task03/miniList/MiniList.java
@@ -0,0 +1,90 @@
+package com.miniTask.task03.miniList;
+
+
+
+public class MiniList<T> {
+    private int size;
+    public int getSize() {
+        return size;
+    }
+    public MiniList() {
+        size = 0;
+    }
+    Node<T> head;
+    Node<T> last;
+
+    public void addLast(T value) {
+        if (this.head == null) {
+            Node<T> node = new Node<T>(value);
+            this.head = node;
+            this.last = node;
+        } else {
+            Node<T> oldLast = last;
+            last.setNext(new Node<T>(value));
+            this.last = last.getNext();
+            this.last.setPrev(oldLast);
+
+        }
+        size++;
+    }
+
+    public T removeFirst() {
+        if (this.head == null) {
+            throw new IllegalArgumentException();
+        }
+        T removeValue = this.head.data();
+        size--;
+        this.head = this.head.getNext();
+        this.head.setPrev(null);
+        return removeValue;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder string = new StringBuilder("[");
+        Node<T> temp = head;
+        while (temp != null) {
+            string.append(temp.data());
+
+            if (temp.getNext() != null) {
+                string.append(",");
+            }
+            temp = temp.getNext();
+        }
+        string.append("]");
+        return string.toString();
+    }
+
+        ///  НОДА
+    private class Node<T> {
+        private T value;
+        private Node<T> next;
+        private Node<T> prev;
+
+        public Node(T value) {
+            this.value = value;
+        }
+
+        public T data() {
+            return value;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setPrev(Node<T> prev) {
+            this.prev = prev;
+        }
+
+        public Node<T> getPrev() {
+            return prev;
+
+        }
+    }
+}
+
