3f5f6a9b071f5b2c365dc2747379e0fd0b6982b9
georgiyelbaf@gmail.com <georgiyelbaf@gmail.com>
2025-05-18 21:04:00 +0300

    [MiniTasks 03] task 05 Unmodifiable

---
+++ src/main/java/com/miniTask/task03/Utils.java
@@ -0,0 +1,10 @@
+package com.miniTask.task03;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Utils<T> {
+    public  List<T> copyUnmodifiable(List<T> src){
+        return Collections.unmodifiableList(src);
+    }
+}
